b71dea16e12e3d8528f9a8cd49c9f79ac9ca8f9c
lahlaliwassim2 <wlahlali34@gmail.com>
2022-10-20 11:11:25 +0100

    implimenter la fonction de validation sur le user controlleur

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,11 +1,17 @@
 const UserModel =require('../models/user.model')
+const ValidatorRegister = require('../validation/Register')
 const Register = async (req,res)=>{
     try {
-        req.body.role="USER"
-        await  UserModel.create(req.body)
-        res.status(200).json({message:'ok'})
+        const {isvalid ,errors} = ValidatorRegister(req.body)
+        if(!isvalid){
+            res.status(404).json(errors)
+        }else{
+            req.body.role="USER"
+            await  UserModel.create(req.body)
+            res.status(200).json({message:'ok'})
+        }      
     } catch (error) {
-        res.status(400).json({merrsage : error})
+        res.status(400).json({message : error})
     }
 }
 
