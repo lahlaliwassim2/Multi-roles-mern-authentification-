62138082ff78c2b4648fb7fcf40ce479bd25f81f
lahlaliwassim2 <wlahlali34@gmail.com>
2022-10-21 16:05:45 +0100

    test token

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
-const { Register, Login } = require('../controllers/users.controllers');
+const { Register, Login, Test } = require('../controllers/users.controllers');
 var router = express.Router();
 
 /* GET home page. */
 router.post('/register',Register  );
 router.post('/login',Login  );
+
+
+
+
+
+
+
+
+
+//Test
+router.get('/test',Test)
 
 module.exports = router;
