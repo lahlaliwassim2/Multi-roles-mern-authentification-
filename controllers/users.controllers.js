b769ad7165ec4c0a5aa8d36db0b29d1d0624a162
lahlaliwassim2 <wlahlali34@gmail.com>
2022-10-21 10:06:09 +0100

    ajouter le jsonwebtoken

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -2,6 +2,9 @@ const UserModel = require('../models/user.model')
 const ValidatorRegister = require('../validation/Register')
 const ValidatorLogin = require('../validation/login')
 const bcrypt = require('bcryptjs');
+const  jwt  = require('jsonwebtoken');
+
+require('dotenv').config()
 
 
 
@@ -40,6 +43,12 @@ const Register = async (req, res) => {
         })
     }
 }
+/**
+ * It checks if the user exists in the database, if it does, it compares the password with the one in
+ * the database, if it matches, it sends a response, if it doesn't, it sends an error message.
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 const Login = async (req, res) => {
     const {
         errors,
@@ -61,10 +70,18 @@ const Login = async (req, res) => {
                             errors.password = "incorect password"
                             res.status(404).json(errors)
                         } else {
-                            res.send('oke')
+                           var token = jwt.sign({
+                            name: user.name,
+                            email: user.email,
+                            role:user.role
+                           },
+                           process.env.PRIVATE, { expiresIn: '1h' });
+                           res.status(200).json({
+                            message:"mzyan",
+                            token:token
+                           })
                         }
                     })
-
                 }
             })
            
