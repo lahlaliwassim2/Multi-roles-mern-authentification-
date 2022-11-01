5383e14b45c7e1442c7e3994a06f88c7aa80ee29
lahlaliwassim2 <wlahlali34@gmail.com>
2022-11-01 15:33:29 +0100

    ajouter une div pour l affichage de l erreure s il existe

--- client/src/components/Inputs.js
+++ client/src/components/Inputs.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import className from 'classnames'
 
-function inputs({name,type,label,icon,onChangeHandler}) {
+function inputs({name,type,label,icon,onChangeHandler,errors}) {
   return (
     <div className=" mb-3">
     <label  className="form-label">{label}</label>
     <div className="input-group">
       <span className="input-group-text" ><i className={icon}></i></span>
-      <input type={type} name={name} className="form-control" onChange={onChangeHandler}/>
+      <input type={type} name={name} className={className("form-control",{"is-invalid":errors})} onChange={onChangeHandler}/>
     </div>
+    {
+      errors && (<div class="alert alert-danger" role="alert">
+      {errors}
+</div>)
+    }
 </div>
   )
 }
